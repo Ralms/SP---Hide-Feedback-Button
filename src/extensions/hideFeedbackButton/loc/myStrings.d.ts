f36d0c7b307a396ccdcc979ee8457c0fb81bd1be
Ricardo Marramaque <ralmslb@gmail.com>
2018-03-14 01:06:38 +0000

    First initial commit

---
+++ src/extensions/hideFeedbackButton/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IHideFeedbackButtonApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'HideFeedbackButtonApplicationCustomizerStrings' {
+  const strings: IHideFeedbackButtonApplicationCustomizerStrings;
+  export = strings;
+}
